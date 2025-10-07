b24ce6d712b362fdcd7c4a6b2755c132100fe857
Jerd220891 <jerd220891@gmail.com>
2025-10-07 09:28:31 +0000

    landing page jerd22

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,12 +1,11 @@
-function Card() {
+function Card({img, text, title}) {
     return (
         <>
             <div class="card" style={{width: "18rem"}}>
-                <img src="https://carriloutdoor.com/blog/wp-content/uploads/2024/01/armas-cortas-que-es-tipos-calibres.jpg" class="card-img-top" alt="..." />
+                <img src={img} class="card-img-top" alt="..." />
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">{title}</h5>
+                    <p class="card-text">{text}</p>
                 </div>
             </div>
         </>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -14,16 +14,16 @@ const Home = () => {
 			<div className="container">
 				<div className="row">
 					<div className="col">
-						<Card />
+						<Card img="https://plus.unsplash.com/premium_photo-1671489203034-fc619a2de3bf?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" title= "Santiago Benabeu" text="Campo de futbol del madrid"/>
 					</div>
 					<div className="col">
-						<Card />
+						<Card img="https://plus.unsplash.com/premium_photo-1671489203034-fc619a2de3bf?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" title= "Mestalla" text="Campo de futbol del valencia"/>
 					</div>
 					<div className="col">
-						<Card />
+						<Card img="https://plus.unsplash.com/premium_photo-1671489203034-fc619a2de3bf?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" title= "Camp nou" text="Campo de futbol del Barcelona"/>
 					</div>
 					<div className="col">
-						<Card />
+						<Card img="https://plus.unsplash.com/premium_photo-1671489203034-fc619a2de3bf?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" title= "Sanchez pijuan" text="Campo de futbol del sevilla"/>
 					</div>
 				</div>
 			</div>
